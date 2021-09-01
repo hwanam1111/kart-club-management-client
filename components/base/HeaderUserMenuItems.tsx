fe609dabb7f559cbf5088896593ca9cf51f84259
LEEJUN KIM <hwanam1111@naver.com>
2021-09-01 15:31:48 +0900

    Fix: remove not use import.

--- components/base/HeaderUserMenuItems.tsx
+++ components/base/HeaderUserMenuItems.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { RootState } from '../../store/reducers';
 import useGetMyInformation from '../../hooks/useGetMyInformation';
 
 const HeaderUserMenuItemsWrapper = styled.ul`
