8215a8dd8ff720166a2250712121264f5c9333d0
LEEJUN KIM <hwanam1111@naver.com>
2021-09-02 19:52:34 +0900

    Add: navigation menu.

--- components/base/NavigationMenu.tsx
+++ components/base/NavigationMenu.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { AiOutlineProfile } from 'react-icons/ai';
+import { AiOutlineHome, AiOutlineProfile, AiOutlineUnorderedList } from 'react-icons/ai';
+import { HiOutlineUserGroup } from 'react-icons/hi';
+import { SiGroupon, SiGoogletagmanager } from 'react-icons/si';
 import styled from 'styled-components';
 
+import useGetMyInformation from '../../hooks/useGetMyInformation';
 import NavigationMenuMain from './NavigationMenuMain';
 
 const NavMenuWrapper = styled.ul`
@@ -10,55 +13,76 @@ const NavMenuWrapper = styled.ul`
   overflow-y: auto;
 `;
 
+const SeparateLine = styled.hr`
+  display: block;
+  border: 1px solid #eee;
+`;
+
 function NavigationMenu() {
+  const { myInformation } = useGetMyInformation();
+
   return (
     <NavMenuWrapper>
-      <NavigationMenuMain
-        title="내 정보 관리"
-        icon={<AiOutlineProfile />}
-        subMenu={[
-          { menuName: '프로필 관리', menuLink: '/users/profile' },
-          { menuName: '클럽 관리', menuLink: '/users/club' },
-        ]}
-        menuLink={null}
-      />
-      <NavigationMenuMain
-        title="등록된 클럽 목록"
-        icon={<AiOutlineProfile />}
-        subMenu={null}
-        menuLink="/clubs"
-      />
-      <NavigationMenuMain
-        title="테스트"
-        icon={<AiOutlineProfile />}
-        subMenu={null}
-        menuLink="/clubs"
-      />
-      <NavigationMenuMain
-        title="테스트2222"
-        icon={<AiOutlineProfile />}
-        subMenu={null}
-        menuLink="/clubs"
-      />
-      <NavigationMenuMain
-        title="리스트"
-        icon={<AiOutlineProfile />}
-        subMenu={[
-          { menuName: '프로필 관리', menuLink: '/users/profile' },
-          { menuName: '메뉴 3 2 12', menuLink: '/users/club' },
-          { menuName: '메뉴 3 2 13', menuLink: '/users/club' },
-          { menuName: '메뉴 3 2 1 4', menuLink: '/users/club' },
-        ]}
-        menuLink={null}
-      />
-      <NavigationMenuMain
-        title="리스트입니다아"
-        icon={<AiOutlineProfile />}
-        subMenu={[
-          { menuName: '메뉴1', menuLink: '/users/profile' },
-        ]}
-        menuLink={null}
-      />
+      {myInformation && (
+        <>
+          <>
+            <NavigationMenuMain
+              title="홈"
+              icon={<AiOutlineHome />}
+              subMenu={null}
+              menuLink="/"
+            />
+            <NavigationMenuMain
+              title="등록된 클럽 리스트"
+              icon={<HiOutlineUserGroup />}
+              subMenu={null}
+              menuLink="/clubs"
+            />
+          </>
+          {myInformation !== 'no-user-info' && (
+            <>
+              <SeparateLine />
+              <NavigationMenuMain
+                title="내 정보 관리"
+                icon={<AiOutlineProfile />}
+                subMenu={[
+                  { menuName: '프로필 관리', menuLink: '/users/profile' },
+                  { menuName: '내 클럽 관리', menuLink: '/users/club' },
+                ]}
+                menuLink={null}
+              />
+              <SeparateLine />
+              <NavigationMenuMain
+                title="클럽 홈"
+                icon={<SiGroupon />}
+                subMenu={null}
+                menuLink="/club/club-name"
+              />
+              <NavigationMenuMain
+                title="클럽 게시판"
+                icon={<AiOutlineUnorderedList />}
+                subMenu={[
+                  { menuName: '자기소개', menuLink: '/club/club-name/members-introduce' },
+                  { menuName: '자유 게시판', menuLink: '/club/club-name/free-board' },
+                  { menuName: '타임어택', menuLink: '/club/club-name/time-attack' },
+                ]}
+                menuLink={null}
+              />
+              <NavigationMenuMain
+                title="클럽 운영진 메뉴"
+                icon={<SiGoogletagmanager />}
+                subMenu={[
+                  { menuName: '클럽 가입신청자 관리', menuLink: '/club/club-name/management/application-users' },
+                  { menuName: '클럽 가입공지 작성', menuLink: '/club/club-name/management/application-notice' },
+                  { menuName: '테스트표 작성', menuLink: '/club/club-name/management/test' },
+                  { menuName: '클럽원 관리', menuLink: '/club/club-name/management/members' },
+                ]}
+                menuLink={null}
+              />
+            </>
+          )}
+        </>
+      )}
     </NavMenuWrapper>
   );
 }
