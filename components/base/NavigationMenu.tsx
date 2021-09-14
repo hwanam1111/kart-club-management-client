46d3897f746f36ba957704b9be2eebf39474da01
LEEJUN KIM <hwanam1111@naver.com>
2021-09-14 16:40:10 +0900

    Fix: navigation menu.

--- components/base/NavigationMenu.tsx
+++ components/base/NavigationMenu.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { AiOutlineHome, AiOutlineProfile, AiOutlineUnorderedList } from 'react-icons/ai';
+import { BiAddToQueue } from 'react-icons/bi';
 import { HiOutlineUserGroup } from 'react-icons/hi';
 import { SiGroupon, SiGoogletagmanager } from 'react-icons/si';
+import { RiSendPlaneLine } from 'react-icons/ri';
 import styled from 'styled-components';
 
 import useGetMyInformation from '../../hooks/useGetMyInformation';
@@ -52,33 +54,54 @@ function NavigationMenu() {
                 menuLink={null}
               />
               <SeparateLine />
-              <NavigationMenuMain
-                title="클럽 홈"
-                icon={<SiGroupon />}
-                subMenu={null}
-                menuLink="/club/club-name"
-              />
-              <NavigationMenuMain
-                title="클럽 게시판"
-                icon={<AiOutlineUnorderedList />}
-                subMenu={[
-                  { menuName: '자기소개', menuLink: '/club/club-name/members-introduce' },
-                  { menuName: '자유 게시판', menuLink: '/club/club-name/free-board' },
-                  { menuName: '타임어택', menuLink: '/club/club-name/time-attack' },
-                ]}
-                menuLink={null}
-              />
-              <NavigationMenuMain
-                title="클럽 운영진 메뉴"
-                icon={<SiGoogletagmanager />}
-                subMenu={[
-                  { menuName: '클럽 가입신청자 관리', menuLink: '/club/club-name/management/application-users' },
-                  { menuName: '클럽 가입공지 작성', menuLink: '/club/club-name/management/application-notice' },
-                  { menuName: '테스트표 작성', menuLink: '/club/club-name/management/test' },
-                  { menuName: '클럽원 관리', menuLink: '/club/club-name/management/members' },
-                ]}
-                menuLink={null}
-              />
+              {myInformation.clubId ? (
+                <>
+                  <NavigationMenuMain
+                    title="클럽 홈"
+                    icon={<SiGroupon />}
+                    subMenu={null}
+                    menuLink="/club/club-name"
+                  />
+                  <NavigationMenuMain
+                    title="클럽 게시판"
+                    icon={<AiOutlineUnorderedList />}
+                    subMenu={[
+                      { menuName: '자기소개', menuLink: '/club/club-name/members-introduce' },
+                      { menuName: '자유 게시판', menuLink: '/club/club-name/free-board' },
+                      { menuName: '타임어택', menuLink: '/club/club-name/time-attack' },
+                    ]}
+                    menuLink={null}
+                  />
+                  {(myInformation.rating === 'master' || myInformation.rating === 'manager') && (
+                    <NavigationMenuMain
+                      title="클럽 운영진 메뉴"
+                      icon={<SiGoogletagmanager />}
+                      subMenu={[
+                        { menuName: '클럽 가입신청자 관리', menuLink: '/club/club-name/management/application-users' },
+                        { menuName: '클럽 가입공지 작성', menuLink: '/club/club-name/management/application-notice' },
+                        { menuName: '테스트표 작성', menuLink: '/club/club-name/management/test' },
+                        { menuName: '클럽원 관리', menuLink: '/club/club-name/management/members' },
+                      ]}
+                      menuLink={null}
+                    />
+                  )}
+                </>
+              ) : (
+                <>
+                  <NavigationMenuMain
+                    title="클럽 등록하기 (마스터)"
+                    icon={<BiAddToQueue />}
+                    subMenu={null}
+                    menuLink="/club/register"
+                  />
+                  <NavigationMenuMain
+                    title="클럽 가입하기 (클럽원)"
+                    icon={<RiSendPlaneLine />}
+                    subMenu={null}
+                    menuLink="/club/join"
+                  />
+                </>
+              )}
             </>
           )}
         </>
